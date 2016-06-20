4f71f9f3c3a77e612f7b5099790d3985c502a6ef
Guillermo Guerrero <wolf.fox1985@gmail.com>
2016-06-21 00:37:57 +0200

    Added main test. Trying goxc.

--- main.go
+++ main.go
@@ -6,17 +6,26 @@ import (
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/devcows/share/cmd"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	// Log as JSON instead of the default ASCII formatter.
 	log.SetFormatter(&log.JSONFormatter{})
 
-	// Output to stderr instead of stdout, could also be a file.
-	log.SetOutput(os.Stderr)
+	if os.Getenv("SHARE_MODE") == "release" {
+		// Only log the warning severity or above.
+		log.SetLevel(log.WarnLevel)
+		gin.SetMode(gin.ReleaseMode)
 
-	// Only log the warning severity or above.
-	log.SetLevel(log.InfoLevel)
+		//TODO: Output to file instead of stdout.
+		//log.SetOutput(os.Stderr)
+	} else {
+		log.SetLevel(log.DebugLevel)
+
+		// Output to stderr instead of stdout, could also be a file.
+		log.SetOutput(os.Stderr)
+	}
 
 	if err := cmd.RootCmd.Execute(); err != nil {
 		fmt.Println(err)
