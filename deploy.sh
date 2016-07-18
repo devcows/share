adaa5f089283828048ee782dc0ddabb32221c84e
Guillermo Guerrero <wolf.fox1985@gmail.com>
2016-07-18 09:07:06 +0200

    Fixed serve folder root. Deploy refactors.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 go get github.com/laher/goxc
+git checkout master
 goxc bump
 git add .goxc.json
 git commit -m "Updated version."
+git push
 goxc
--- lib/file_server.go
+++ lib/file_server.go
@@ -3,12 +3,55 @@ package lib
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 	"os"
+	"sort"
 	"strings"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/julienschmidt/httprouter"
 )
+
+type byName []os.FileInfo
+
+func (s byName) Len() int           { return len(s) }
+func (s byName) Less(i, j int) bool { return s[i].Name() < s[j].Name() }
+func (s byName) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+
+func dirList(w http.ResponseWriter, folderPath string, prefix string) error {
+	f, err := os.Open(folderPath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	dirs, err := f.Readdir(-1)
+	if err != nil {
+		// TODO: log err.Error() to the Server.ErrorLog, once it's possible
+		// for a handler to get at its Server via the ResponseWriter. See
+		// Issue 12438.
+		//log.Error(w, "Error reading directory", http.StatusInternalServerError)
+		return err
+	}
+	sort.Sort(byName(dirs))
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "<pre>\n")
+	for _, d := range dirs {
+		name := d.Name()
+		if d.IsDir() {
+			name += "/"
+		}
+		// name may contain '?' or '#', which must be escaped to remain
+		// part of the URL path, and not indicate the start of a query
+		// string or fragment.
+		url := url.URL{Path: name}
+		fmt.Fprintf(w, "<a href=\"%s\">%s</a>\n", prefix+"/"+url.String(), name)
+	}
+	fmt.Fprintf(w, "</pre>\n")
+
+	return nil
+}
 
 func FileServerGET(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	filePathURL := ps.ByName("filePathURL")
@@ -25,13 +68,12 @@ func FileServerGET(w http.ResponseWriter, r *http.Request, ps httprouter.Params)
 	}
 
 	if info, err := os.Stat(server.Path); err == nil && info.IsDir() {
-		log.Debug("Returning to folder: " + server.Path)
-
 		if len(paths) == 2 {
-			http.ServeFile(w, r, server.Path)
+			log.Debug("Returning to folder: " + server.Path)
+			dirList(w, server.Path, server.UUID)
 		} else {
 			filePath := server.Path + string(os.PathSeparator) + strings.Join(paths[2:len(paths)], string(os.PathSeparator))
-			log.Debug("Returning to file: " + filePath)
+
 			http.ServeFile(w, r, filePath)
 		}
 	} else {
